9ff4ccb573cf11c8732be53bf399f6fef5a14d8f
Matthieu Coudron <mattator@gmail.com>
2018-10-22 12:10:29 +0900

    Started rockspec support, converting source into multiple srcs = []

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ with import <nixpkgs> {};
 
 let
   luaEnv = lua5_2.withPackages( ps: [ ps.luarocks-nix ] );
-  luarocksLocalCopy = "/home/teto/luarocks/";
+  luarocksLocalCopy = "$PWD";
 in
 luarocks-nix.overrideAttrs (oa: {
 
@@ -13,6 +13,7 @@ luarocks-nix.overrideAttrs (oa: {
   # buildInputs =  [ git luaEnv nix-prefetch-scripts ];
   src = ./.;
 
+  # Not needed anymore ?
   # shellHook=''
   #   export LUA_PATH="${luarocksLocalCopy}/src/?.lua;''${LUA_PATH:-}"
   #   '';
